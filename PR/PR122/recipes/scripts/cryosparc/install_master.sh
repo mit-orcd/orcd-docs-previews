c8588a9f09b476ff04348ca55740266fe91009b4
secorey <secorey@users.noreply.github.com>
2025-09-05 13:34:02 +0000

    Deploying to main from  @ 72096611b550daf89758dd0017fb1be1d348529c 🚀

--- PR/PR122/recipes/scripts/cryosparc/install_master.sh
+++ PR/PR122/recipes/scripts/cryosparc/install_master.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+module load miniforge
+
 cd $CRYOSPARC_WORKDIR/cryosparc_master
 
 DB_PATH=$CRYOSPARC_WORKDIR/cryosparc_database
+PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
 
 ./install.sh --license $CRYOSPARC_LICENSE_ID \
              --hostname $(hostname) \
              --dbpath $DB_PATH \
-             --port 61000
+             --port $PORT
