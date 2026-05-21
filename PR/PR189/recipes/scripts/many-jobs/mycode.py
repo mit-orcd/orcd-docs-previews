96e10330a07c6a6940386750a4ed74d7c022351b
jm0rray <jm0rray@users.noreply.github.com>
2026-05-21 15:34:23 +0000

    Deploying to main from  @ 76489d6c4315d35ef0c8880c8e014296f01b7601 🚀

---
+++ PR/PR189/recipes/scripts/many-jobs/mycode.py
@@ -0,0 +1,4 @@
+import sys              # Import system package 
+n = int(sys.argv[1])    # Use the first input argument from command line
+m = n - 1               # do some computation
+print(n, m)             # Print the result 
